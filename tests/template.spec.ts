f122ce2bbc3b13dd35a5f0bdc3608ab221b23446
naopeke <naopeke@protonmail.com>
2024-09-04 11:32:54 +0200

    update

--- tests/template.spec.ts
+++ tests/template.spec.ts
@@ -8,6 +8,10 @@ import { test, expect } from '@playwright/test';
 
 const password = process.env.PASSWORD;
 
+/**
+ * https://playwright.dev/docs/api/class-test
+ * Declares a beforeAll hook that is executed once per worker process before all tests.
+ */
 test.beforeAll(async ({ playwright }) => {
     test.skip(
       !!process.env.PROD,
@@ -17,7 +21,11 @@ test.beforeAll(async ({ playwright }) => {
     // create a db connection
     // reuse a sign in state
 });
-  
+
+/**
+ * https://playwright.dev/docs/api/class-test
+ * Declares a beforeEach hook that is executed before each test.
+ */
 test.beforeEach(async ({ page }, testInfo) => {
     console.log(`Running ${testInfo.title}`);
     // open a URL
@@ -27,11 +35,19 @@ test.beforeEach(async ({ page }, testInfo) => {
     // load params
 });
 
+/**
+ * https://playwright.dev/docs/api/class-test
+ * Declares an afterAll hook that is executed once per worker after all tests.
+ */
 test.afterAll(async ({ page }, testInfo) => {
     console.log('Test file completed.');
     // close a DB connection
 });
 
+/**
+ * https://playwright.dev/docs/api/class-test
+ * Declares an afterEach hook that is executed after each test.
+ */
 test.afterEach( async ({ page }, testInfo) => {
     console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
 
@@ -40,10 +56,35 @@ test.afterEach( async ({ page }, testInfo) => {
     // clean up all the data we created for this test through API calls
 });
 
+
+/**
+ * https://playwright.dev/docs/api/class-test#test-describe
+ * Declares a group of tests.
+
+    test.describe(title, callback)
+    test.describe(callback)
+    test.describe(title, details, callback)
+
+ * 
+ * 
+ * https://playwright.dev/docs/api/class-test
+ * Declares a skipped test group, similarly to test.describe(). Tests in the skipped group are never run.
+    スキップする。オプション。
+    test.describe.skip(title, callback)
+    test.describe.skip(title)
+    test.describe.skip(title, details, callback)
+
+    test.describe.only('Test Case', () =>{...}も可能)
+ */
 // test.describe('Test Case', () => {
 // test.describe.only('Test Case', () => {
 test.describe.skip('Test Case', () => {
     test('Test Scenario One', async ({ page }) => {
+        /**
+         * https://playwright.dev/docs/api/class-test
+         * Declares a test step that is shown in the report.
+         * オプション
+         */
         await test.step('Step One', async () => {
             // ...
         });
