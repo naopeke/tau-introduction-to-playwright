fa3d5e85e237e52bfca2f96e8409806e35bce85f
naopeke <naopeke@protonmail.com>
2024-09-04 10:40:25 +0200

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -36,20 +36,36 @@ test('get started link', async ({ page }) => {
  * 
  */
 
-// test.only('check Java page', async ({ page }) => {
-test('check Java page', async ({ page }) => {
-
+test ('check Java page', async ({page}) =>{
+  // open the page
   await page.goto('https://playwright.dev');
-
-  await page.getByRole('link', {name: 'Get started'}).click();
+  // click 'get started'
+  await page.getByRole('link', {name : 'Get started'}).click();
+  //mouse hover the language dropdown in Node.js button
   await page.getByRole('button', {name: 'Node.js'}).hover();
-  await page.getByText('Java', { exact:true }).click();
-  // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
-
+  // click 'java' and matches exactly
+  await page.getByText('Java', {exact:true}).click();
+  // check the URL
   await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
-  await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
+  // check if "Installing Playwright" is not being displayed
+  await expect(page.getByText('Installing Playwright', {exact: true})).not.toBeVisible();
+  // check if "Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation."
+  await expect(page.getByText("Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.")).toBeVisible();
+})
+// // test.only('check Java page', async ({ page }) => {
+// test('check Java page', async ({ page }) => {
 
-  const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
-  await expect(page.getByText(javaDescription)).toBeVisible();
+//   await page.goto('https://playwright.dev');
 
-});
+//   await page.getByRole('link', {name: 'Get started'}).click();
+//   await page.getByRole('button', {name: 'Node.js'}).hover();
+//   await page.getByText('Java', { exact:true }).click();
+//   // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
+
+//   await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+//   await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
+
+//   const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+//   await expect(page.getByText(javaDescription)).toBeVisible();
+
+// });
